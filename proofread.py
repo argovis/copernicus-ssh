e8696ebadd375c237ecf3f63b841610050581025
billmills <you@example.com>
2023-06-22 20:03:00 -0400

    proofreading script for weekly means to mongo

--- proofread.py
+++ proofread.py
@@ -1,32 +1,44 @@
 import datetime, xarray, random, math
+from pymongo import MongoClient
 
-def timewindow(center, radius):
-    # given a string specifying the central date in the format "1993-02-07T00:00:00.000Z",
-    # produce a list of strings for the days +- radius around that date in the format yyyymmdd
+def tidylon(longitude):
+    # map longitude on [0,360] to [-180,180], required for mongo indexing
+    if longitude <= 180.0:
+        return longitude;
+    else:
+        return longitude-360.0;
 
-    centerdatetime = datetime.datetime.strptime(center, "%Y-%m-%dT%H:%M:%SZ")
-    datestrings = []
-    for i in range(-1*radius, radius+1):
-    	datestrings.append( (centerdatetime + datetime.timedelta(days=i)).strftime("%Y%m%d") )
+# db connection
+client = MongoClient('mongodb://database/argo')
+db = client.argo
 
-    return datestrings
-
-dates = timewindow("1993-01-17T00:00:00Z", 3)
-timeidx = 1; # time index the timewindow date corresponds to
-xars = [xarray.open_dataset(f"data/dt_global_twosat_phy_l4_{date}_vDT2021.nc", decode_times=False, mask_and_scale=False) for date in dates]
-means = xarray.open_dataset("data/ssh_mean_1993.nc", decode_times=False)
+batchfiles = ["data/ssh_mean_9394.nc","data/ssh_mean_9596.nc","data/ssh_mean_9798.nc","data/ssh_mean_9900.nc","data/ssh_mean_0102.nc","data/ssh_mean_0304.nc","data/ssh_mean_0506.nc","data/ssh_mean_0708.nc","data/ssh_mean_0910.nc","data/ssh_mean_1112.nc","data/ssh_mean_1314.nc","data/ssh_mean_1516.nc","data/ssh_mean_1718.nc","data/ssh_mean_1920.nc","data/ssh_mean_2122.nc"]
 
 while True:
-	lat = math.floor(random.random()*720)
+	lat = 175 # math.floor(random.random()*720)
 	lon = math.floor(random.random()*1440)
 
-	total = 0
-	nobs = 0
-	for xar in xars:
-		meas = xar['sla'][0][lat][lon].to_dict()['data']
-		if meas != -2147483647:
-			total += meas*0.0001
-			nobs += 1
+	# pick the corresponding mongo doc
+	upstream = xarray.open_dataset(batchfiles[0], decode_times=False, mask_and_scale=False)
+	latitude = upstream['latitude'][lat].to_dict()['data']
+	longitude = tidylon(upstream['longitude'][lon].to_dict()['data'])
+	_id = str(longitude) + "_" + str(latitude)
+	doc = db.copernicusSLA.find_one({"_id": _id})
 
-	if means['sla'][timeidx][lat][lon].to_dict()['data'] != -999.9 and (not math.isclose(total/nobs, means['sla'][timeidx][lat][lon].to_dict()['data'], abs_tol=1e-5) or not math.isclose(nobs, means['nobs'][timeidx][lat][lon].to_dict()['data'], abs_tol=1e-5)):
-		print(total/nobs, means['sla'][timeidx][lat][lon].to_dict()['data'], nobs, means['nobs'][timeidx][lat][lon].to_dict()['data'])
+	# reconstruct the list of means
+	means = []
+	for f in batchfiles:
+		xar = xarray.open_dataset(f, decode_times=False, mask_and_scale=False)
+		sla = xar.isel(latitude=lat, longitude=lon)['sla'].to_dict()['data']
+		nobs = xar.isel(latitude=lat, longitude=lon)['nobs'].to_dict()['data']
+		m = [sla[i] if nobs[i]==7 else -999.9 for i in range(len(sla))]
+		means += m
+
+	
+	# compare with tolerance
+	for i in range(len(means)):
+		if (means[i] != -999.9 and round(means[i],4) != round(doc['data'][0][i],4)) or (means[i] == -999.9 and not math.isnan(doc['data'][0][i])):
+			print("mismatch for profile " + _id + ' at ' + str(i))
+
+
+	
